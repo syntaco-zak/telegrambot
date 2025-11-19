866898bb0485b2b4fe3a3f38d80d2b1dbe3ecdaa
= <=>
2025-11-19 11:10:51 +0000

    set up all files

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+import django
+from user_data.models import UserData
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from dotenv import load_dotenv
+from user_data.models import UserData
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "telegrambot.settings")
+django.setup()
+load_dotenv()
+
+async def start(update, context):
+    await update.message.reply_text("Welcome! I am your bot!")
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello, your name is {update.effective_user.first_name}')
+
+app = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & filters.Regex(r"(?i)^hello, what is my name\??$"), hello))
+app.run_polling()
